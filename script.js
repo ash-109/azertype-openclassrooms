f755cb0b923cb32bb12b91b0c5d067a0ef19aa3b
Ashvin Antyah <a.antyah@gmail.com>
2024-07-26 19:41:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-const listeMots = ["Cachalot", "Pétunia", "Serviette"]
-console.log(listeMots);
-let score = 0 
-const motUtilisateur = prompt('Tapez : ' + listeMots[0])
+const listeMots = ["Cachalot", "Pétunia", "Serviette"];
+const listePhrases = [
+  "Pas de panique !",
+  "La vie, l’univers et le reste",
+  "Merci pour le poisson",
+];
+let score = 0;
 
-if (motUtilisateur === listeMots[0]
-) {
-    score++
-    console.log(score);
+//choix de la liste avec laquelle l'utilisateur veut jouer
+let choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
+
+// repeter le prompt tant que l'utilisateur n'a pas choisi entre "mots" et "phrases"
+ while (choix !== "mots" && choix !== "phrases") {
+    choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
+}
+
+
+// Si l'utilisateur choisi la liste de mots
+if (choix==="mots") {
+   for (let i = 0; i < listeMots.length; i++) {
+  let motUtilisateur = prompt("Entrez le mot " + listeMots[i]);
+  if (motUtilisateur === listeMots[i]) {
+    score++;
+  } 
+}
+console.log("Votre score est de " + score + " sur " + listeMots.length);
+
+}else {
+    
+    for (let i = 0; i < listePhrases.length; i++) {
+        let phraseUtilisateur = prompt("Entrez la phrase : " + listePhrases[i])
+        if (phraseUtilisateur === listePhrases[i]) {
+         
+            score++
+        }
+    }
+    console.log("Votre score est de " + score + " sur " + listePhrases.length)
 }
