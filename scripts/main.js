65c0ea54f72a801327d18aea6a7e06d0eaf7dc9c
Ashvin Antyah <a.antyah@gmail.com>
2024-08-01 20:11:03 +0200

    update js

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,1 @@
-let motJoueur = document.getElementById('motJoueur')
-console.log(motJoueur);
-
-let btnValidation = document.getElementById('btnValidation')
-console.log(btnValidation);
-
-let affichageScore= document.querySelector('.score span')
-console.log(affichageScore);
-
-let btnRadio = document.querySelectorAll('.choixListe input')
-console.log(btnRadio);
-
 lancerJeu()
--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 function afficherResultat(score, nbMotsProposes) {
-  affichageScore.innerText = `${score} / ${nbMotsProposes}
-`;
+  let affichageScore = document.getElementById(".score span");
+  affichageScore.innerText = `${score} / ${nbMotsProposes}`;
 }
 
 function afficherProposition(proposition) {
@@ -10,27 +10,47 @@ function afficherProposition(proposition) {
 
 function lancerJeu() {
   let score = 0;
-  let nbMotsProposes = 0;
   let i = 0;
+  let listeProposition = listeMots;
 
   let btnValidation = document.getElementById("btnValidation");
   let motJoueur = document.getElementById("motJoueur");
-  afficherProposition(listeMots[i]);
-
+  afficherProposition(listeProposition[i]);
+  // Gestion du click sur le bouton valider
   btnValidation.addEventListener("click", () => {
     console.log(motJoueur.value);
-    if (motJoueur.value === listeMots[i]) {
+    if (motJoueur.value === listeProposition[i]) {
       score++;
     }
     i++;
     afficherResultat(score, i);
     motJoueur.value = "";
-    if (listeMots[i] === undefined) {
+    if (listeProposition[i] === undefined) {
       afficherProposition("Le jeu est fini");
       btnValidation.disabled = true;
     } else {
-      afficherProposition(listeMots[i]);
+      afficherProposition(listeProposition[i]);
     }
   });
+
+  // Gestion de l'événement change sur les boutons radios.
+
+  let inputRadio = document.querySelectorAll("input[type=radio]");
+  console.log(inputRadio);
+
+  for (let index = 0; index < inputRadio.length; index++) {
+    inputRadio[index].addEventListener("change", (event) => {
+      // Si c'est le premier élément qui a été modifié, alors nous voulons
+      // jouer avec la listeMots.
+      if (event.target.value === "mots") {
+        listeProposition = listeMots;
+      } else {
+        // Sinon nous voulons jouer avec la liste des phrases
+        listeProposition = listePhrases;
+      }
+      // Et on modifie l'affichage en direct.
+      afficherProposition(listeProposition[i]);
+    });
+  }
   afficherResultat(score, i);
 }
